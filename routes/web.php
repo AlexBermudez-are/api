181f5e97e6e8766acd5f6c2df0f627837b177926
Alexis Bermúdez <71720889+AlexBermudez-are@users.noreply.github.com>
2024-05-21 19:33:00 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\CuponsController;
+use App\Http\Controllers\OfferController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/csrf-token', function () {
+    return response()->json(['token' => csrf_token()]);
 });
+
+Route::get('/user/cupons', [CuponsController::class, 'getCupons']);
+Route::post('/user/cupon/create', [CuponsController::class, 'createCupon']);
+Route::put('/user/cupon/{id}', [CuponsController::class, 'redeemCoupon']); 
+
+Route::get('/user/{id}', [UserController::class, 'getUserUnique']); //
+Route::post('/user/register', [UserController::class, 'register']); //
+Route::post('/user/login', [UserController::class, 'login']);//
+
+Route::get('/offers', OfferController::class);//
